6a261e25efc76a8e00b8e5304a48c479b191a46c
David J DeBonis <davidjdebonis@gmail.com>
2020-09-08 07:09:30 -0600

    test ostream overload in main header

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 using namespace std;
 
+ostream operator<<(const ostream & out, const Point &pnt){
+    out << "(" << pnt.getX() << ", " << pnt.getY() << ")";
+    return out;
+}
+
 int main(){
     cout << "This is the start of the program" << endl;
 
